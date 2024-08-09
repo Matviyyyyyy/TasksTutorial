48b6b968205074befe6d0e3fe1c2d75d104a0011
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-08-09 10:14:48 +0300

    Start project

--- tasking/forms.py
+++ tasking/forms.py
@@ -1,7 +1,7 @@
-from django import forms
-from tasking.models import Task
+    from django import forms
+    from tasking.models import Task
 
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ["title", "description", "status", "priority", "due_date", "creator"]
+    class TaskForm(forms.ModelForm):
+        class Meta:
+            model = Task
+            fields = ["title", "description", "status", "priority", "due_date", "creator"]
